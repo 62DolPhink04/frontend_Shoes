3d2c8e33e18ae13417c1a1d577d541278f425f4d
Nhut <nhut@example.com>
2025-11-01 18:49:21 +0700

    first commit

--- src/layout/mainLayout.jsx
+++ src/layout/mainLayout.jsx
@@ -44,13 +44,19 @@ const mainLayout = () => {
           <div>
             <h3 className="text-lg font-bold">Support</h3>
             <ul className="mt-2 space-y-1">
-              <li>
+              <li className="text-secondary">
                 <a href="https://www.facebook.com/">FaceBook</a>
               </li>
-              <li>FAQ</li>
-              <li>Site feedback</li>
-              <li>Expert Help</li>
-              <li>Resources and Guides</li>
+              <li className="text-secondary">
+                <a href="https://zalo.me/pc"></a>
+              </li>
+              <li className="text-secondary">
+                <a href="https://www.tiktok.com">Tiktok</a>
+              </li>
+              <li className="text-secondary">
+                <a href="https://www.instagram.com/">Instagram</a>
+              </li>
+              <li>Feeback</li>
             </ul>
           </div>
           <div>
